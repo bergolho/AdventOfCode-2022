904f807f96f679e5dae567a92e18b72836610fae
bergolho <berg@ice.ufjf.br>
2022-12-28 13:18:37 -0300

    Day 8 is clear

--- C++/Day-8/main.cpp
+++ C++/Day-8/main.cpp
@@ -11,13 +11,67 @@
 #include <string>
 
 void print_grid (std::vector<std::vector<uint32_t>> grid) {
-	printf("%u %u\n",grid.size(),grid[0].size());
+	printf("%lu %lu\n",grid.size(),grid[0].size());
 	for (uint32_t i = 0; i < grid.size(); i++) {
 		for (uint32_t j = 0; j < grid[i].size(); j++) {
 			printf("%u ", grid[i][j]);
 		}
 		printf("\n");
 	}
+}
+
+uint32_t calculate_score (std::vector<std::vector<uint32_t>> grid, const int i, const int j) {
+	uint32_t nlin = grid.size();
+	uint32_t ncol = grid[0].size();
+	uint32_t cur_height = grid[i][j];
+	
+	std::vector<uint32_t> north;
+	for (int k = i-1; k >= 0; k--) {
+		north.push_back(grid[k][j]);
+	}
+	std::vector<uint32_t> south;
+	for (int k = i+1; k < (int)nlin; k++) {
+		south.push_back(grid[k][j]);
+	}
+	std::vector<uint32_t> east;
+	for (int k = j+1; k < (int)ncol; k++) {
+		east.push_back(grid[i][k]);
+	}
+	std::vector<uint32_t> west;
+	for (int k = j-1; k >= 0; k--) {
+		west.push_back(grid[i][k]);
+	}
+	
+	uint32_t north_score = 0;
+	for (int k = 0; k < (int)north.size(); k++) {
+		north_score++;
+		if (cur_height <= north[k]) {
+			break;
+		}
+	}
+	uint32_t south_score = 0;
+	for (int k = 0; k < (int)south.size(); k++) {
+		south_score++;
+		if (cur_height <= south[k]) {
+			break;
+		}
+	}
+	uint32_t east_score = 0;
+	for (int k = 0; k < (int)east.size(); k++) {
+		east_score++;
+		if (cur_height <= east[k]) {
+			break;
+		}
+	}
+	uint32_t west_score = 0;
+	for (int k = 0; k < (int)west.size(); k++) {
+		west_score++;
+		if (cur_height <= west[k]) {
+			break;
+		}
+	}
+
+	return north_score*west_score*east_score*south_score;
 }
 
 bool check_cell (std::vector<std::vector<uint32_t>> grid, const int i, const int j) {
@@ -30,11 +84,11 @@ bool check_cell (std::vector<std::vector<uint32_t>> grid, const int i, const int
 		north.push_back(grid[k][j]);
 	}
 	std::vector<uint32_t> south;
-	for (int k = i+1; k < nlin; k++) {
+	for (int k = i+1; k < (int)nlin; k++) {
 		south.push_back(grid[k][j]);
 	}
 	std::vector<uint32_t> east;
-	for (int k = j+1; k < ncol; k++) {
+	for (int k = j+1; k < (int)ncol; k++) {
 		east.push_back(grid[i][k]);
 	}
 	std::vector<uint32_t> west;
@@ -43,28 +97,28 @@ bool check_cell (std::vector<std::vector<uint32_t>> grid, const int i, const int
 	}
 
 	bool is_north_ok = true;
-	for (int k = 0; k < north.size(); k++) {
+	for (int k = 0; k < (int)north.size(); k++) {
 		if (cur_height <= north[k]) {
 			is_north_ok = false;
 			break;
 		}
 	}
 	bool is_south_ok = true;
-	for (int k = 0; k < south.size(); k++) {
+	for (int k = 0; k < (int)south.size(); k++) {
 		if (cur_height <= south[k]) {
 			is_south_ok = false;
 			break;
 		}
 	}
 	bool is_east_ok = true;
-	for (int k = 0; k < east.size(); k++) {
+	for (int k = 0; k < (int)east.size(); k++) {
 		if (cur_height <= east[k]) {
 			is_east_ok = false;
 			break;
 		}
 	}
 	bool is_west_ok = true;
-	for (int k = 0; k < west.size(); k++) {
+	for (int k = 0; k < (int)west.size(); k++) {
 		if (cur_height <= west[k]) {
 			is_west_ok = false;
 			break;
@@ -94,13 +148,12 @@ void read_input (std::vector<std::string> &in) {
 void solve_part_1 (std::vector<std::string> in) {
 	std::vector<std::vector<uint32_t>> grid;
 	parse_grid(in,grid);
-	//print_grid(grid);
 
 	uint32_t nlin = grid.size();
 	uint32_t ncol = grid[0].size();
 	uint32_t ans = (nlin*ncol)-((nlin-2)*(ncol-2));
-	for (int i = 1; i < nlin-1; i++) {
-		for (int j = 1; j < ncol-1; j++) {
+	for (int i = 1; i < (int)nlin-1; i++) {
+		for (int j = 1; j < (int)ncol-1; j++) {
 			bool is_visible = check_cell(grid, i, j);
 			if (is_visible) {
 				//printf("Visible\n\n",i, j);
@@ -111,16 +164,30 @@ void solve_part_1 (std::vector<std::string> in) {
 	printf("[Answer 1] %u\n", ans);
 }
 
-void solve_part_2 () {
-	 // Your solution goes here ...
+void solve_part_2 (std::vector<std::string> in) {
+	std::vector<std::vector<uint32_t>> grid;
+	parse_grid(in,grid);
+
+	uint32_t nlin = grid.size();
+	uint32_t ncol = grid[0].size();
+	uint32_t ans = 0;
+	for (int i = 1; i < (int)nlin-1; i++) {
+		for (int j = 1; j < (int)ncol-1; j++) {
+			uint32_t scenic_score = calculate_score(grid, i, j);
+			if (scenic_score > ans) {
+				ans = scenic_score;
+			}		
+		}
+	}
+	printf("[Answer 2] %u\n", ans);
 }
 
 int main () {
 	std::vector<std::string> input;
 	read_input(input);
 
-	solve_part_1(input);
-	solve_part_2();
+	//solve_part_1(input);
+	solve_part_2(input);
 
 	return 0;
 }
